3b72b4130138bded99236195e5523f58e2cd5172
Alberto Palma <jesus.palma247910@potros.itson.edu.mx>
2025-04-14 14:38:05 -0700

    inicial

---
+++ FerrePlus_Negocio/src/main/java/com/mycompany/ferreplus_negocio/FerrePlus_Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ferreplus_negocio;
+
+/**
+ *
+ * @author Beto_
+ */
+public class FerrePlus_Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
